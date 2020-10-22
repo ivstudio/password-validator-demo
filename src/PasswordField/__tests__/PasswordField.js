f416cd248485d8bffe6521b8ffe5bb7eff922e6c
Ignacio Villamar <iggy@ivstudio.com>
2020-10-22 07:29:07 -0400

    Update test handlers and test desc

--- src/PasswordField/__tests__/PasswordField.js
+++ src/PasswordField/__tests__/PasswordField.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { render, screen, fireEvent, cleanup } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import PassWordField from "../PasswordField";
-
-afterEach(cleanup);
 
 test("Renders a password input", () => {
   render(<PassWordField />);
@@ -10,32 +9,32 @@ test("Renders a password input", () => {
   expect(input).toHaveAttribute("type", "password");
 });
 
-test("Enabled submit with valid input", () => {
-  render(<PassWordField />);
-  const input = screen.getByLabelText(/password/i);
-  const button = screen.getByText(/submit/i);
-  expect(button).toBeDisabled();
-  fireEvent.change(input, { target: { value: "Lx=1pwsd" } });
-  expect(button).not.toBeDisabled();
-});
-
-test("Helper text is correctly render", () => {
+test("Helper text and success banner visibility", async () => {
   render(<PassWordField />);
   const input = screen.getByLabelText(/password/i);
   expect(screen.getByRole("alert")).toBeInTheDocument();
   expect(screen.queryByRole("banner")).toBeNull();
-  fireEvent.change(input, { target: { value: "Lx=1pwsd" } });
+  await userEvent.type(input, "Lx=1pwsd");
   expect(screen.queryByRole("alert")).toBeNull();
   expect(screen.getByRole("banner")).toBeInTheDocument();
 });
 
-test("Toggle show password", () => {
+test("Enable submit with valid password", async () => {
+  render(<PassWordField />);
+  const input = screen.getByLabelText(/password/i);
+  const button = screen.getByText(/submit/i);
+  expect(button).toBeDisabled();
+  await userEvent.type(input, "Lx=1pwsd");
+  expect(button).not.toBeDisabled();
+});
+
+test("Toggle password visibility", () => {
   render(<PassWordField />);
   const input = screen.getByLabelText(/password/i);
   const button = screen.getByText(/show/i);
   expect(input).toHaveAttribute("type", "password");
   expect(button).toHaveTextContent(/show/i);
-  fireEvent.click(button);
+  userEvent.click(button);
   expect(input).toHaveAttribute("type", "text");
   expect(button).toHaveTextContent(/hide/i);
 });
